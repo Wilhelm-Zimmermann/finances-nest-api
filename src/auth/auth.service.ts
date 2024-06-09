112845b12395796e173454d23134e2d41ab296f6
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2024-06-09 14:06:38 -0300

    guardian, strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { compare } from "bcrypt";
+import { User } from "src/users/models/User";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+
+  login() {
+    return "ola";
+  }
+
+  async validateUser(username: string, password: string) {
+    const user = await this.usersRepository.findOne({
+      where: {
+        username,
+      },
+    });
+
+    return await compare(password, user.password);
+  }
+}
